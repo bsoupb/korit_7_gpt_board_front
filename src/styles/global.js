22722e4a2aa1ab58163cc61bc2dbc3454ecaad96
bsoupb <qotn0327@naver.com>
2025-02-21 17:55:46 +0900

    board front 로그인, 회원가입

---
+++ src/styles/global.js
@@ -0,0 +1,29 @@
+import { css } from "@emotion/react";
+
+export const global = css`
+
+    @import url('https://fonts.googleapis.com/css2?family=Black+Han+Sans&family=Noto+Sans+KR:wght@100..900&family=Noto+Sans:ital,wght@0,100..900;1,100..900&display=swap');
+
+    * {
+        color: #333333;
+    }
+
+    html, body, #root {
+        margin: 0;
+        padding: 0;
+        height: 100vh;
+        overflow: auto;
+
+        font-family: "Noto Sans KR", serif;
+        font-size: 62.5%;   /* 1rem을 10px로 변환 */
+    }
+
+    #root {
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        background-color: #fafafa;
+    }
+
+
+`;
